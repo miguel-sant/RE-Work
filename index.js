54e6cd5c082b4501c17cd20186cf72564e814b8b
miguel-sant <miguelcfcs@hotmail.com>
2026-03-05 15:50:56 -0300

    [Adicionando personagem ao mapa]

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
-canvas.width = 1600
-canvas.height = 900
+canvas.width = 1024;
+canvas.height = 576;
 
 c.rect(0, 0, canvas.width, canvas.height);
-c.fillStyle = 'white';
+c.fillStyle = "white";
 
-const image = new Image;
-image.src = './img/Pellet Town.png'
+const image = new Image();
+image.src = "./img/Pellet Town.png";
 
-image.onload= () => {                      //Somente quando a imagem for carregada eu desenho ela
-    c.drawImage(image, -900,-550)
+const playerImage = new Image();
+playerImage.src = "./img/playerDown.png";
+
+class Sprite {
+    constructor() {
+        
+    }
 }
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.drawImage(image, -745, -555);
+    c.drawImage(
+        playerImage,
+        0,
+        0, // aqui é o recorte
+        playerImage.width / 4,
+        playerImage.height,
+
+        canvas.width / 2 - playerImage.width / 4 + 12,
+        canvas.height / 2 - playerImage.height / 4, // aqui é como está atual
+        playerImage.width / 4,
+        playerImage.height,
+    )
+}
+animate();
+window.addEventListener("keydown", function (e) {
+    switch (e.key) {
+        case "w":
+            console.log("w");
+            break;
+        case "a":
+            console.log("a");
+            break;
+        case "s":
+            console.log("s");
+            break;
+        case "d":
+            console.log("d");
+            break;
+    }
+});
+};
